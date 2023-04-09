066aff863886353017752ad0aa2284eec4edeca0
peterbell215 <peter.bell215@gmail.com>
2023-04-09 21:08:31 +0100

    Added walking player

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,7 @@ impl State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
         self.player.render(ctx);
     }
